fddade879fe6bf9532ffbd295fee3346266fd7a4
Sebastiandg7 <13395979+sebastiandg7@users.noreply.github.com>
2022-05-17 11:36:40 -0500

    fix: pass removeRequestOrigin flag to proxy server

--- bin/lcp.js
+++ bin/lcp.js
@@ -1,27 +1,44 @@
 #!/usr/bin/env node
 
-var lcp = require('../lib/index.js');
-var commandLineArgs = require('command-line-args');
+var lcp = require("../lib/index.js");
+var commandLineArgs = require("command-line-args");
 
 var optionDefinitions = [
-  { name: 'port', alias: 'p', type: Number, defaultValue: 8010 },
+  { name: "port", alias: "p", type: Number, defaultValue: 8010 },
   {
-    name: 'proxyPartial',
+    name: "proxyPartial",
     type: String,
-    defaultValue: '/proxy'
+    defaultValue: "/proxy",
   },
-  { name: 'proxyUrl', type: String },
-  { name: 'credentials', type: Boolean, defaultValue: false },
-  { name: 'origin', type: String, defaultValue: '*' },
-  { name: 'removeRequestOrigin', type: Boolean, defaultValue: false }
+  { name: "proxyUrl", type: String },
+  { name: "credentials", type: Boolean, defaultValue: false },
+  { name: "origin", type: String, defaultValue: "*" },
+  { name: "removeRequestOrigin", type: Boolean, defaultValue: false },
 ];
 
 try {
   var options = commandLineArgs(optionDefinitions);
-  if (!options.proxyUrl) {
-    throw new Error('--proxyUrl is required');
+  const {
+    port,
+    proxyUrl,
+    proxyPartial,
+    credentials,
+    origin,
+    removeRequestOrigin,
+  } = options;
+
+  if (!proxyUrl) {
+    throw new Error("--proxyUrl is required");
   }
-  lcp.startProxy(options.port, options.proxyUrl, options.proxyPartial, options.credentials, options.origin);
+
+  lcp.startProxy(
+    port,
+    proxyUrl,
+    proxyPartial,
+    credentials,
+    origin,
+    removeRequestOrigin
+  );
 } catch (error) {
   console.error(error);
 }
